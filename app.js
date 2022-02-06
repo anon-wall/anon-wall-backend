ffa331e5394e16775390764a8cc70ed5afe3c85e
Sangmin Na <sangmin.iam@gmail.com>
2022-02-06 12:46:51 +0900

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+require("dotenv").config();
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const logger = require("morgan");
 
-var app = express();
+const usersRouter = require("./routes/users");
+const connectToMongoDB = require("./loaders/mongooseLoader");
+
+const app = express();
+
+connectToMongoDB();
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500);
+
+  if (req.app.get("env") === "development") {
+    res.json({
+      result: err.result || "error",
+      message: err.message,
+      stack: err.stack,
+    });
+
+    return;
+  }
+
+  res.json({
+    result: err.result || "error",
+    message: err.message,
+  });
+});
 
 module.exports = app;
