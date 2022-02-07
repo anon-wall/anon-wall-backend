b1e3159be4ebc94477c5950795e417b82aa79ed0
merkyuri <kyuri.park@kakao.com>
2022-02-08 01:43:37 +0900

    Feat: get counsel-story list

--- routes/counsels.js
+++ routes/counsels.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {
   createCounsel,
   getCounsel,
+  getCounselList,
 } = require("../controllers/counsels.controller");
 const { checkObjectId } = require("../middlewares/validateObjectId");
 
 router.post("/", createCounsel);
+router.get("/", getCounselList);
 router.get("/:counsel_id", checkObjectId, getCounsel);
 
 module.exports = router;
