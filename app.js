0d0e6be76d6c4d84ef17d28c88cb33fd05020de4
sojungLIMM <thwid9897@gmail.com>
2022-02-07 21:08:33 +0900

    feat: add cors options

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ const app = express();
 connectMongoDB();
 
 app.use(logger("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
