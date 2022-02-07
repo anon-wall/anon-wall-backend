c7a055597cfc9b2ab2dece72b19a91667471db5a
sojungLIMM <thwid9897@gmail.com>
2022-02-07 20:24:21 +0900

    feat: get counsel detail

--- controllers/counsels.controller.js
+++ controllers/counsels.controller.js
@@ -1,7 +1,7 @@
 const createError = require("http-errors");
 
 const Counsel = require("../models/Counsel");
-const { RESPONSE } = require("../constants");
+const { RESPONSE, MESSAGE } = require("../constants");
 
 exports.createCounsel = async (req, res, next) => {
   try {
@@ -21,6 +21,28 @@ exports.createCounsel = async (req, res, next) => {
       data: null,
     });
   } catch (err) {
+    next(createError(err));
+  }
+};
+
+exports.getAll = async (req, res, next) => {
+  try {
+    const { counsel_id } = req.params;
+
+    const counsel = await Counsel.findById(counsel_id)
+      .populate("counselors")
+      .lean();
+
+    res.status(201).json({
+      result: RESPONSE.SUCCESS,
+      data: counsel,
+    });
+  } catch (err) {
+    if (err.name === "CastError") {
+      next(createError.BadRequest(MESSAGE.BADREQUEST));
+      return;
+    }
+
     next(createError(err));
   }
 };
