6a89b2de225ae3f583d4af6a104456fa8ec2a5ff
sojungLIMM <thwid9897@gmail.com>
2022-02-08 12:04:19 +0900

    feat: update Counsel

--- routes/counsels.js
+++ routes/counsels.js
@@ -5,11 +5,13 @@ const {
   createCounsel,
   getCounsel,
   getCounselList,
+  updateCounsel,
 } = require("../controllers/counsels.controller");
 const { checkObjectId } = require("../middlewares/validateObjectId");
 
 router.post("/", createCounsel);
 router.get("/", getCounselList);
+router.post("/:counsel_id/counselors", checkObjectId, updateCounsel);
 router.get("/:counsel_id", checkObjectId, getCounsel);
 
 module.exports = router;
