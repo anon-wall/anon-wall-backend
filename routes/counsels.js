2362914245862775cd4e65b8db0b6a821a410027
sojungLIMM <thwid9897@gmail.com>
2022-02-09 10:45:47 +0900

    feat: make pagination middleware
    chore: change lines

--- routes/counsels.js
+++ routes/counsels.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const { checkObjectId } = require("../middlewares/validateObjectId");
+const { pagination } = require("../middlewares/pagination");
 const {
   createCounsel,
   getCounselList,
   updateCounsel,
   getCounsel,
 } = require("../controllers/counsels.controller");
-const { checkObjectId } = require("../middlewares/validateObjectId");
 
 router.post("/", createCounsel);
-router.get("/", getCounselList);
+router.get("/", pagination, getCounselList);
 router.post("/:counsel_id/counselors", checkObjectId, updateCounsel);
 router.get("/:counsel_id", checkObjectId, getCounsel);
 
