69143f78e1a7fc943a35265d5468447a1d2d8dc3
merkyuri <kyuri.park@kakao.com>
2022-02-12 00:28:19 +0900

    feat: check is objectId valid

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCounselor, updateUser } = require("../controllers/users.controller");
+const {
+  getCounselor,
+  updateUser,
+  updateCounselorSchedule,
+} = require("../controllers/users.controller");
 const { checkObjectId } = require("../middlewares/validateObjectId");
 
 router.get("/:user_id", checkObjectId, getCounselor);
 router.patch("/:user_id", checkObjectId, updateUser);
+router.post(
+  "/:user_id/counselor/availableDates",
+  checkObjectId,
+  updateCounselorSchedule
+);
 
 module.exports = router;
