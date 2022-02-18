27c2b9ff7699d80ee1b269f1625ddbd018255561
merkyuri <kyuri.park@kakao.com>
2022-02-18 23:57:54 +0900

    chore: add verifyToken

--- routes/counsels.js
+++ routes/counsels.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { checkObjectId } = require("../middlewares/validateObjectId");
+const { verifyToken } = require("../middlewares/authorization");
 const {
   createCounsel,
   getCounselList,
@@ -12,20 +13,20 @@ const {
   getSchedules,
 } = require("../controllers/counsels.controller");
 
-router.post("/", createCounsel);
+router.post("/", verifyToken, createCounsel);
 router.get("/", getCounselList);
-router.get("/reserved", getReservedCounselList);
-router.get("/schedules", getSchedules);
-router.get("/:counsel_id", checkObjectId, getCounsel);
+router.get("/reserved", verifyToken, getReservedCounselList);
+router.get("/schedules", verifyToken, getSchedules);
+router.get("/:counsel_id", verifyToken, checkObjectId, getCounsel);
 router.post(
   "/:counsel_id/counselors/:user_id",
-
+  verifyToken,
   checkObjectId,
   updateCounsel
 );
 router.post(
   "/:counsel_id/counselors",
-
+  verifyToken,
   checkObjectId,
   updateCounselors
 );
